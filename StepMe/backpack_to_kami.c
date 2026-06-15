b2d5638a9dc5ac6abed95e4f5ec74fb546e22d8c
Amir-B42 <147371415+Amir-B42@users.noreply.github.com>
2026-06-15 11:55:58 +0200

    Create backpack_to_kami.c

---
+++ StepMe/backpack_to_kami.c
@@ -0,0 +1,12 @@
+/*
+variables du sac à dos : Capacité C, valeurs des objets V, poids des objets P
+problème -> V max
+
+variables Kami : Le Graphe G, nb de couleurs N
+problème -> nb de couts optimaux (séquence importe ?)
+*/
+
+
+
+
+
